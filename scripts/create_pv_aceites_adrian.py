332b066ed147f5b2a860e1f28bb4b9020417f823
kxmwnzbzhn-spec <kxmwnzbzhn@privaterelay.appleid.com>
2026-06-20 16:44:41 -0600

    add SCENT

--- scripts/create_pv_aceites_adrian.py
+++ scripts/create_pv_aceites_adrian.py
@@ -40,6 +40,7 @@ ATTRS=[
  {"id":"GTIN","values":[{"name":EAN}]},
  {"id":"ITEM_CONDITION","values":[{"name":"Nuevo"}]},
  {"id":"COUNTRY_OF_ORIGIN","values":[{"name":"México"}]},
+ {"id":"SCENT","values":[{"name":"Sándalo"}]},
  {"id":"ESSENTIAL_OIL_NAME","values":[{"name":"Piedra Viva"}]},
 ]
 
